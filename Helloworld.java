d5d88884dfc1cf214aa122e54d7dd5bab09b847c
Ashwini-Gorde <ashwinigorde@gmail.com>
2020-04-29 10:31:40 +0000

    Restored the file from second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,10 +1,6 @@
-//simple message
 public class Helloworld{
         public static void main(String[] args){
-
         System.out.println("Hello, world");
-       // prints "welcome to git" in the terminal window.
-        System.out.println("welcome to git");
         }
 }
 
